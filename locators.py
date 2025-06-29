b5dfd93a502cc0da695ebde39d3cb63663bf5429
JaneM00 <evg.mamikonyan@yandex.ru>
2025-06-30 01:51:53 +0300

    Create locators.py

---
+++ locators.py
@@ -0,0 +1,12 @@
+# locators.py
+
+# Базовый URL API
+BASE_URL = "https://qa-scooter.praktikum-services.ru"
+
+# Пути эндпоинтов для курьеров
+COURIER_REGISTER = "/api/v1/courier"
+COURIER_LOGIN = "/api/v1/courier/login"
+
+# Пути эндпоинтов для заказов
+ORDERS_CREATE = "/api/v1/orders"
+ORDERS_LIST = "/api/v1/orders"
