83703d45f756b7b817923b8586a7ee69cc83e9fa
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-03 16:34:55 +0300

    Update test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,7 +1,7 @@
 # test_order.py
 import requests
 import pytest
-from locators import BASE_URL, ORDERS_CREATE, ORDERS_LIST
+from urls import BASE_URL, ORDERS_CREATE, ORDERS_LIST
 
 class TestOrder:
     def get_sample_order_data(self):
@@ -32,11 +32,8 @@ class TestOrder:
         order_body_cleaned = {k: v for k, v in order_body.items() if v is not None}
 
         response = requests.post(f"{BASE_URL}{ORDERS_CREATE}", json=order_body_cleaned)
-        
         assert response.status_code in [200, 201]
-        
         resp_json = response.json()
-        
         # Проверка наличия track номера в ответе.
         assert isinstance(resp_json.get("track"), int)
 
