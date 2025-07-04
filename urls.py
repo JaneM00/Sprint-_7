b3c793007c087464f1161747ba969ed743b4e299
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-04 17:46:07 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,12 +1,8 @@
 # urls.py
+BASE_URL = "https://qa-scooter.praktikum-services.ru/api"
 
-# Базовый URL API
-BASE_URL = "https://qa-scooter.praktikum-services.ru"
+COURIER_REGISTER = "/v1/courier"
+COURIER_LOGIN = "/v1/courier/login"
 
-# Пути эндпоинтов для курьеров
-COURIER_REGISTER = "/api/v1/courier"
-COURIER_LOGIN = "/api/v1/courier/login"
-
-# Пути эндпоинтов для заказов
-ORDERS_CREATE = "/api/v1/orders"
-ORDERS_LIST = "/api/v1/orders"
+ORDERS_CREATE = "/v1/orders"
+ORDERS_LIST = "/v1/orders"
