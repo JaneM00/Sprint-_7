b4bf8ced63261eafabe18e34e8b7c7f963d6da2d
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-07 14:56:37 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,21 +1,13 @@
 # conftest.py
 import pytest
-import requests
-from helpers import generate_courier_data, generate_order_data
-from urls import BASE_URL, COURIER_REGISTER
 
-@pytest.fixture(scope='function')
-def create_courier():
-    data = generate_courier_data()
-    response = requests.post(f"{BASE_URL}{COURIER_REGISTER}", json=data)
-    # Проверка успешного создания или существования курьера
-    assert response.status_code in [201, 409]
-    return data
+@pytest.fixture(scope='session')
+def base_url():
+    return "https://qa-scooter.praktikum-services.ru"
 
-@pytest.fixture(scope='function')
-def create_order():
-    from helpers import generate_order_data
-    order_data = generate_order_data()
-    response = requests.post(f"{BASE_URL}/v1/orders", json=order_data)
-    assert response.status_code in [200, 201]
-    return order_data
+@pytest.fixture(scope='session')
+def endpoints():
+    return {
+        "create_order": "/api/v1/orders",
+        "list_orders": "/api/v1/orders",
+    }
