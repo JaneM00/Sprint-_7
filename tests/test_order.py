fc9cfe6b0701199d9fc4244780a77d825f31683f
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-08 23:41:20 +0300

    Update test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,48 +1,40 @@
 # test_order.py
 import requests
 import pytest
+import allure
 from locators import BASE_URL, ORDERS_CREATE, ORDERS_LIST
+from data import get_sample_order_data
 
 class TestOrder:
-    def get_sample_order_data(self):
-        return {
-            'firstName': 'Ivan',
-            'lastName': 'Ivanov',
-            'address': 'Lenina street',
-            'metroStation': '4',
-            'phone': '+79991112233',
-            'rentTime': 5,
-            'deliveryDate': '2023-10-10',
-            'comment': '',
-            'color': [],  # по умолчанию пустой список
-            'price': 1000,
-        }
-
     @pytest.mark.parametrize("colors", [
         [],
         ["BLACK"],
         ["GREY"],
         ["BLACK", "GREY"]
     ])
+    @allure.title("Создание заказа с цветами: {colors}")
     def test_create_order_with_colors(self, colors):
-        order_body = self.get_sample_order_data()
+        order_body = get_sample_order_data()
         order_body['color'] = colors
 
         # Удаляем ключи со значением пустого списка или None, если нужно
         order_body_cleaned = {k: v for k, v in order_body.items() if v not in [None, [], ""]}
 
-        response = requests.post(f"{BASE_URL}{ORDERS_CREATE}", json=order_body_cleaned)
-        
-        assert response.status_code in [200, 201], f"Unexpected status code: {response.status_code}"
-        
-        resp_json = response.json()
-        
-        # Проверка наличия track номера в ответе и его типа
-        assert "track" in resp_json, "Response JSON does not contain 'track'"
-        assert isinstance(resp_json["track"], int), f"'track' is not an int: {resp_json['track']}"
+        with allure.step("Отправка POST-запрос на создание заказа"):
+            response = requests.post(f"{BASE_URL}{ORDERS_CREATE}", json=order_body_cleaned)
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code in [200, 201], f"Unexpected status code: {response.status_code}"
+        with allure.step("Проверка наличия 'track' в ответе и его типа"):
+            resp_json = response.json()
+            assert "track" in resp_json, "Response JSON does not contain 'track'"
+            assert isinstance(resp_json["track"], int), f"'track' is not an int: {resp_json['track']}"
 
+    @allure.title("Получение списка заказов")
     def test_get_orders(self):
-        response = requests.get(f"{BASE_URL}{ORDERS_LIST}")
-        assert response.status_code == 200
-        orders_list = response.json()
-        assert isinstance(orders_list, list)
+        with allure.step("Отправка GET-запрос на получение списка заказов"):
+            response = requests.get(f"{BASE_URL}{ORDERS_LIST}")
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200
+        with allure.step("Проверка типа возвращаемого объекта (список)"):
+            orders_list = response.json()
+            assert isinstance(orders_list, list)
